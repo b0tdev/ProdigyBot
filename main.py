07bfc03f124c1e6e59a4c24b5abb492551ff7998
I'm GrooT <51871154+b0tdev@users.noreply.github.com>
2019-07-21 21:50:59 +0530

    Updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#pylint:disable=E0001
+# Main Code
 import discord
 from discord.ext import commands
 
@@ -177,4 +177,4 @@ async def user(ctx, member:discord.User = None):
     role = member.top_role
     await ctx.channel.send(f"{pronoun} name is {name}. {pronoun} status is {status}. They joined at {joined}. {pronoun} rank is {role}.")
     
-bot.run("NjAyNDc2MDA0MTMyMjU3Nzky.XTRZwQ.YLxY-tiXTmGCCC6mBzj4Taw0wik")
+bot.run(os.getenv("TOKEN"))
